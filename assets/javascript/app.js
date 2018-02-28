2409fc0a6fc662a1f11e56f25505d8b0056991dd
RSReitz <notthedba@outlook.com>
2018-02-28 11:25:05 -0500

    Eliminated .json file for storage.  Will revist later.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,57 @@
-// function readJsonFile(file) {
-
-//     var response = "";
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.overrideMimeType("application/json");
-//     rawFile.open("GET", file, true);
-//     rawFile.onreadystatechange = function() {
-//         if (rawFile.readyState === 4 && rawFile.status == "200") {
-//             // callback(rawFile.responseText);
-//             response = rawFile.responseText;
-
-//         }
-//     }
-//     console.log(response);
-//     rawFile.send(null);
-//     var data = JSON.parse(response);
-//     return data;
-// }
-
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
+var data = {
+    "trivia": {
+        "1": {
+            "question": "Which of these movies is NOT based on a board game?",
+            "choices": ["Ouija",
+                "Jumanji",
+                "Battleship",
+                "Clue"
+            ],
+            "answer": 1,
+            "fact": "Jumanju is about a boardgame of the same name, and there was a Jumanji game released in 1995 around the same time as the movie, but it was actually based on a 1981 novel by Chris Van Allsburg."
+        },
+        "2": {
+            "question": "Which of these is the cheapest group of properties in Monopoly?",
+            "choices": ["Dark Purple", "Light Purple", "Red", "Light Blue", "Dark Blue"],
+            "answer": 0,
+            "fact": "The Dark Purple properties - Mediterranean and Baltic Ave. - are the cheapest properties in Monopoly; they only cost $120 to buy.  The other colors, in order, are:  Light Blue - $320; Light Purple (Maroon/Pink) - $440; Orange - $560; Red - $700; Dark Blue - $750; Yellow - $800; Green- $920."
+        },
+        "3": {
+            "question": "Three of the following are different names for the same game:  Which one is a different game?",
+            "choices": ["Shogun", "Samurai Swords", "Ikusa", "James Clavell's Shogun"],
+            "answer": 3,
+            "fact": "Although there is a boardgame called James 'Clavell's Shogun', it is a different game.   The original name for Ikusa was 'Shogun', but it was later changed to Samurai Swords due to copyright issues with James Clavell's novel of the same name, then later changed to it's current name of Ikusa. "
+        },
+        "4": {
+            "question": "Which of these is NOT a common characteristic of 'Euro' style board games?",
+            "choices": ["Tile Placement", "Set Collection", "Player Elimination", "Area Control", "Worker Placement"],
+            "answer": 2,
+            "fact": "A Eurogame, also called a German-style or Euro-style board game, generally have indirect player interaction and abstract physical components. Euro-style games emphasize strategy while downplaying luck and conflict. They tend to have economic themes rather than military and usually keep all the players in the game until it ends."
+        },
+        "5": {
+            "question": "Which of these games features dice drafting as a game mechanic?",
+            "choices": ["Seasons", "7 Wonders", "Dominion", "Button Men"],
+            "answer": 0,
+            "fact": "In Seasons, the first player rolls a number of dice.  Each player in turn then selects one of the rolled values, and passes the rest of the dice along to the next player.  This is known as dice drafting."
+        },
+        "999": {
+            "question": "Freebie:  The answer is zero.",
+            "choices": ["0", "1", "2", "3"],
+            "answer": 0,
+            "fact": "Freebies are fun!"
         }
     }
-    rawFile.send(null);
-}
+};
+
+
+var count = Object.keys(data.trivia).length;
+var trivia;
 
 $(document).ready(function() {
-    var count = 0;
-    var scene;
-    // readTextFile("assets/data/data.json", function(text, count) {
-    //     data = JSON.parse(text);
-    //     count = Object.keys(data.trivia).length;
-    //     console.log(data);
-    // });
 
-    // var trivia = readJsonFile("assets/data/data.json");
+    console.log(count);
 
-    // console.log(count);
-    // console.log(Object.keys(trivia.trivia).length);
-    // console.log(trivia.trivia[1].fact);
-    $.getJSON("assets/data/trivia.json", function(data) {
-        //     scene = data;
-        console.log(data);
-    });
+    console.log(data.trivia[1].fact);
 
 
-    // console.log(scene);
 });
